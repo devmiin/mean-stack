8ba2382e6e4fbaa611aeeebc707f752113def42b
devmiin <meepooh27@hotmail.com>
2018-06-13 22:28:08 +0700

    implement service

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -6,28 +6,30 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ServiceService {
+  headers = new Headers();
+  options;
 
   constructor(
     private http: Http,
-  ) { }
+  ) {
+    const auth = btoa('nayar:mydumbpassword');
+
+    this.headers.append('Authorization', `Basic ${auth}`);
+    this.headers.append('Access-Control-Allow-Origin', '*');
+    this.headers.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    this.headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+    this.options = new RequestOptions({ headers: this.headers });
+  }
 
   /**
    * creates a base-64 encoded ASCII string from a "string" of binary data.
    * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
    */
   getInvoice() {
-    const auth = btoa('nayar:mydumbpassword');
-
-    const headers = new Headers();
-    headers.append('Authorization', `Basic ${auth}`);
-    headers.append('Access-Control-Allow-Origin', '*');
-    headers.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
-    const options = new RequestOptions({ headers: headers });
-
     const endpoint = `http://localhost:3000/api/`;
 
-    return this.http.get('http://localhost:3000/api', options).pipe(
+    return this.http.get('http://localhost:3000/api', this.options).pipe(
       map(res => res.json())
     );
   }
